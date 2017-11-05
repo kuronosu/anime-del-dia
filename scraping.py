58d69642f0cd2e5f62b183c72ca1803bdad4bd43
Felipe Martinez <andresfelipe.2031@gmail.com>
2017-11-04 21:41:34 -0500

    Primeros pasos para el envio del e-mail

--- scraping.py
+++ scraping.py
@@ -1,19 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib3
+import time
+import shutil
+import os
+import smtplib
 
 def scraping():
     url = 'www.animeflv.net'
-    rensponse = requests.get("https://" + url)
-    soup = BeautifulSoup(rensponse.content, 'html.parser')
+    response = requests.get("https://" + url)
+    soup = BeautifulSoup(response.content, 'html.parser')
     anime = soup.find_all('div', {'class':'AnimeDia'})
     title = anime[0].find('span', {'class': 'Title'}).string#find('strong')
-    image = 'https://' + url + anime[0].find('img')['src']
-    image_name = image.split("/")[len(image.split("/"))-1]
-    synp = anime[0].find('div', {'class': 'Synopsis',}).find('div', {'class': 'Description'}).string
-    print(image_name)
-    print(title)
-    return anime
+    image_url = 'https://' + url + anime[0].find('img')['src']
+    image_name = image_url.split("/")[len(image_url.split("/"))-1]
+    with open(image_name, 'wb') as f:
+        r = requests.get(image_url, stream=True)
+        shutil.copyfileobj(r.raw, f)
+    synopsis = anime[0].find('div', {'class': 'Synopsis',}).find('div', {'class': 'Description'}).string
+    return title, image_url, image_name, synopsis
+
+def main(seconds):
+    REMITENTE = "<andresfelipe.2031@gmail.com>" 
+    DESTINATARIO = "<andresfelipe.2031@gmail.com>" 
+    ASUNTO = "Anime del día"
+    MENSAJE_BASE = """
+        <h1>El anime del dia es: <strong> %s </strong> </h1>
+        <figure>
+            <img src="%s" alt="%s">
+        </figure>
+        <span>sinopis: </span>
+        <p> %s </p>
+        """
+    BASE_DIR = os.path.dirname(__file__)
+    # smtp = smtplib.SMTP('localhost')
+    # smtp.sendmail(REMITENTE, DESTINATARIO, mensaje)
+    while True:
+        title, image_url, image_name, synopsis = scraping()
+        mensaje = MENSAJE_BASE%(title, image_url, title, synopsis)
+        print(mensaje)
+        time.sleep(seconds)
+
 
 if __name__ == '__main__':
-    scraping()
+    h = 24
+    m = 60
+    s = 60
+    main(s*m*h)
